b6ad5963a7422daba4d3989707b3ba1b1132bc23
arberito <arbertopalli.important@gmail.com>
2024-01-18 11:41:29 -0500

    strncat

--- C03/ex03/ft_strncat.c
+++ C03/ex03/ft_strncat.c
@@ -1,34 +1,41 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                    :::::::   :::::::       */
-/*   ft_strncat.c                                    :+:   :+: :+:   :+:      */
-/*                                                   +:+   +:+ +:+   +:+      */
-/*   By: artopall | artopall@student.42quebec.co     +#+   +:+ +#+   +:+      */
-/*                                                   +#+   +#+ +#+   +#+      */
-/*   Created: 2024/01/12 06:08:57 by artopall        #+#   #+# #+#   #+#      */
-/*   Updated: 2024/01/12 06:13:43 by artopall         #######   #######.qc    */
+/*                                                     :::::::   :::::::      */
+/*   ft_strncat.c                                     :+:   :+: :+:   :+:     */
+/*                                                    +:+  :+:+ +:+  :+:+     */
+/*   By: artopall <artopall@student.42.qc>            +#+ + +:+ +#+ + +:+     */
+/*                                                    +#+#  +#+ +#+#  +#+     */
+/*   Created: 2024/01/12 06:08:57 by artopall         #+#   #+# #+#   #+#     */
+/*   Updated: 2024/01/18 11:09:28 by artopall          #######   ####### .qc  */
 /*                                                                            */
 /* ************************************************************************** */
+
+unsigned int	ft_strlen(char *str)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		i += 1;
+	}
+	return (i);
+}
 
 char	*ft_strncat(char *dest, char *src, unsigned int nb)
 {
 	unsigned int	i;
-	char			*temp;
+	unsigned int	dest_len;
 
 	i = 0;
-	temp = dest;
-	while (*dest)
-	{
-		dest += 1;
-	}
+	dest_len = ft_strlen(dest);
 	while (*src && i < nb)
 	{
 		
-		*dest = *src;
+		dest[dest_len + i] = *src;
 		i += 1;
 		src += 1;
-		dest += 1;
 	}
-	*dest = 0;
-	return (temp);
+	dest[dest_len + i] = 0;
+	return (dest);
 }
