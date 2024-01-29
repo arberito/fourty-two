9e1e1a96fb8a2bb5a0a49184c3691450f444e944
arberito <arbertopalli.important@gmail.com>
2024-01-29 17:12:14 -0500

    ft_split

--- C09/ex02/ft_split.c
+++ C09/ex02/ft_split.c
@@ -6,13 +6,13 @@
 /*   By: artopall <artopall@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/22 07:09:45 by artopall          #+#    #+#             */
-/*   Updated: 2024/01/28 22:03:38 by artopall         ###   ########.fr       */
+/*   Updated: 2024/01/29 17:11:05 by artopall         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-int ft_is_charset(char c, char *charset)
+int	ft_is_charset(char c, char *charset)
 {
 	while (*charset)
 	{
@@ -25,7 +25,7 @@ int ft_is_charset(char c, char *charset)
 	return (0);
 }
 
-int	ft_strlen(char *str, char *charset)
+int	ft_slen(char *str, char *charset)
 {
 	int	i;
 
@@ -37,10 +37,10 @@ int	ft_strlen(char *str, char *charset)
 	return (i);
 }
 
-int ft_get_size(char *str, char *charset)
+int	ft_get_size(char *str, char *charset)
 {
-	int size;
-	
+	int	size;
+
 	size = 0;
 	while (*str)
 	{
@@ -50,8 +50,8 @@ int ft_get_size(char *str, char *charset)
 			size += 1;
 		while (*str && ft_is_charset(*str, charset))
 			str += 1;
-    }
-    return (size);
+	}
+	return (size);
 }
 
 char	**ft_split(char *str, char *charset)
@@ -63,39 +63,22 @@ char	**ft_split(char *str, char *charset)
 	array = malloc(sizeof(char *) * (ft_get_size(str, charset) + 1));
 	if (array == NULL)
 		return (NULL);
-	index = 0;
+	index = -1;
 	while (*str)
 	{
 		if (!ft_is_charset(*str, charset))
 		{
-			array[index] = malloc(sizeof(char) * (ft_strlen(str, charset) + 1));
-			i = 0;
-			while (ft_is_charset(*str, charset) && *str)
+			array[++index] = malloc(sizeof(char) * (ft_slen(str, charset) + 1));
+			i = -1;
+			while (!ft_is_charset(*str, charset) && *str)
 			{
-				array[index][i]= *str;
+				array[index][++i] = *str;
 				str += 1;
-				i += 1;
 			}
 			array[index][i] = 0;
-			index += 1;
 		}
 		str += 1;
 	}
-	array[index] = 0;
+	array[(ft_get_size(str, charset) + 1)] = 0;
 	return (array);
-}
-
-#include <stdio.h>
-
-int	main()
-{
-	char	**split;
-
-	split = ft_split("hello world", "l ");
-
-	for (int i = 0; split[i]; i++)
-	{
-		printf("[%s]", split[i]);
-	}
-	
 }
