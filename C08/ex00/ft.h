52adf9222431a872d5d61e3068694d008e7750e8
arberito <arbertopalli.important@gmail.com>
2024-01-29 19:37:19 -0500

    repo qui pu le cul

--- C08/ex00/ft.h
+++ C08/ex00/ft.h
@@ -6,13 +6,14 @@
 /*   By: artopall <artopall@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/15 11:27:17 by artopall          #+#    #+#             */
-/*   Updated: 2024/01/28 14:38:38 by artopall         ###   ########.fr       */
+/*   Updated: 2024/01/29 19:35:11 by artopall         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_H
 # define FT_H
 
+void	ft_putchar(char c);
 void	ft_swap(int *a, int *b);
 void	ft_putstr(char *str);
 int		ft_strlen(char *str);
